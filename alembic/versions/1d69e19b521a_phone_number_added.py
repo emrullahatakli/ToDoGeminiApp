b662fab59f181fcf311b8c1723510627e2025ffe
EmrullahAtakli <emrullah9989@gmail.com>
2026-03-12 17:20:08 +0300

    Initial Commit: Todo Gemini App Baslangic

---
+++ alembic/versions/1d69e19b521a_phone_number_added.py
@@ -0,0 +1,25 @@
+"""phone number added
+
+Revision ID: 1d69e19b521a
+Revises: 
+Create Date: 2026-02-23 17:51:30.867600
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '1d69e19b521a'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.add_column('users',sa.Column('phone_number', sa.String(), nullable = True))
+
+def downgrade() -> None:
+    pass
